2fb0a25a186751326e452147e6a031461fc9b1a9
Nina-Hgh <haghighati.narges@gmail.com>
2021-03-10 18:08:56 -0500

    SQL update

---
+++ SQL/Basic/Revising the Select Query I.sql
@@ -0,0 +1,5 @@
+
+/* "*" means all of the columns 
+    WHERE is used for adding condition*/
+
+SELECT * FROM CITY WHERE COUNTRYCODE ="USA" AND POPULATION > 100000;
---
+++ SQL/Basic/Select All.sql
@@ -0,0 +1,3 @@
+/* * means all of the columns*/
+
+SELECT * FROM CITY;
